c70ad6ae34dc77b3968cd996eabc7dab99ee5053
Michael Krasnyk <michael.krasnyk@gmail.com>
2023-03-30 15:03:20 +0200

    Don't compare primaryKey and restoredKey.

--- dist/save/index.js
+++ dist/save/index.js
@@ -39353,11 +39353,11 @@ function saveImpl(stateProvider) {
             }
             // If matched restore key is same as primary key, then do not save cache
             // NO-OP in case of SaveOnly action
-            const restoredKey = stateProvider.getCacheState();
-            if (utils.isExactKeyMatch(primaryKey, restoredKey)) {
-                core.info(`Cache hit occurred on the primary key ${primaryKey}, not saving cache.`);
-                return;
-            }
+            // const restoredKey = stateProvider.getCacheState();
+            // if (utils.isExactKeyMatch(primaryKey, restoredKey)) {
+            //     core.info(`Cache hit occurred on the primary key ${primaryKey}, not saving cache.`);
+            //     return;
+            // }
             const cachePaths = utils.getInputAsArray(constants_1.Inputs.Path, {
                 required: true
             });
